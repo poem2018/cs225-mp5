b3d7201b5e633a6909b9ac72ae7adf283f3a16ab
laphets <i@laphets.com>
2019-04-19 20:17:45 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/mp5.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/laphets/Desktop/CS225/cs225sp19/mp5/cs225/HSLAPixel.cpp" "/Users/laphets/Desktop/CS225/cs225sp19/mp5/cmake-build-debug/CMakeFiles/mp5.dir/cs225/HSLAPixel.cpp.o"
+  "/Users/laphets/Desktop/CS225/cs225sp19/mp5/cs225/PNG.cpp" "/Users/laphets/Desktop/CS225/cs225sp19/mp5/cmake-build-debug/CMakeFiles/mp5.dir/cs225/PNG.cpp.o"
+  "/Users/laphets/Desktop/CS225/cs225sp19/mp5/cs225/lodepng/lodepng.cpp" "/Users/laphets/Desktop/CS225/cs225sp19/mp5/cmake-build-debug/CMakeFiles/mp5.dir/cs225/lodepng/lodepng.cpp.o"
+  "/Users/laphets/Desktop/CS225/cs225sp19/mp5/main.cpp" "/Users/laphets/Desktop/CS225/cs225sp19/mp5/cmake-build-debug/CMakeFiles/mp5.dir/main.cpp.o"
+  "/Users/laphets/Desktop/CS225/cs225sp19/mp5/maptiles.cpp" "/Users/laphets/Desktop/CS225/cs225sp19/mp5/cmake-build-debug/CMakeFiles/mp5.dir/maptiles.cpp.o"
+  "/Users/laphets/Desktop/CS225/cs225sp19/mp5/mosaiccanvas.cpp" "/Users/laphets/Desktop/CS225/cs225sp19/mp5/cmake-build-debug/CMakeFiles/mp5.dir/mosaiccanvas.cpp.o"
+  "/Users/laphets/Desktop/CS225/cs225sp19/mp5/sourceimage.cpp" "/Users/laphets/Desktop/CS225/cs225sp19/mp5/cmake-build-debug/CMakeFiles/mp5.dir/sourceimage.cpp.o"
+  "/Users/laphets/Desktop/CS225/cs225sp19/mp5/tileimage.cpp" "/Users/laphets/Desktop/CS225/cs225sp19/mp5/cmake-build-debug/CMakeFiles/mp5.dir/tileimage.cpp.o"
+  "/Users/laphets/Desktop/CS225/cs225sp19/mp5/util/coloredout.cpp" "/Users/laphets/Desktop/CS225/cs225sp19/mp5/cmake-build-debug/CMakeFiles/mp5.dir/util/coloredout.cpp.o"
+  "/Users/laphets/Desktop/CS225/cs225sp19/mp5/util/util.cpp" "/Users/laphets/Desktop/CS225/cs225sp19/mp5/cmake-build-debug/CMakeFiles/mp5.dir/util/util.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../cs225"
+  "../cs225/catch"
+  "../cs225/lodepng"
+  "../util"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/mp5.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mp5.dir/cs225/lodepng/lodepng.cpp.o"
+  "CMakeFiles/mp5.dir/cs225/HSLAPixel.cpp.o"
+  "CMakeFiles/mp5.dir/cs225/PNG.cpp.o"
+  "CMakeFiles/mp5.dir/util/coloredout.cpp.o"
+  "CMakeFiles/mp5.dir/util/util.cpp.o"
+  "CMakeFiles/mp5.dir/main.cpp.o"
+  "CMakeFiles/mp5.dir/maptiles.cpp.o"
+  "CMakeFiles/mp5.dir/mosaiccanvas.cpp.o"
+  "CMakeFiles/mp5.dir/sourceimage.cpp.o"
+  "CMakeFiles/mp5.dir/tileimage.cpp.o"
+  "mp5.pdb"
+  "mp5"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mp5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
